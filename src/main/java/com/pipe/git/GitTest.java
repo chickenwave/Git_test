72c3a162de846cec6332a0c25af316fe41854fca
pipe <pipetest@xx.com>
2023-10-21 23:07:31 +0800

    idea push  commit

--- src/main/java/com/pipe/git/GitTest.java
+++ src/main/java/com/pipe/git/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
         System.out.println("hello git  hot-fix!");
         System.out.println("hello git  hot-fix!  code master");
         System.out.println("hello git  hot-fix!  code hot-fix");
-
+        System.out.println("hello git  push test");
     }
 }
