4cba18351f390a5144502bf1f1843897bc160e86
pipe <pipetest@xx.com>
2023-10-21 22:48:41 +0800

    idea hot-fix second commit

--- src/main/java/com/pipe/git/GitTest.java
+++ src/main/java/com/pipe/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello git  222!");
         System.out.println("hello git  333!");
         System.out.println("hello git  hot-fix!");
+        System.out.println("hello git  hot-fix!  code hot-fix");
 
     }
 }
